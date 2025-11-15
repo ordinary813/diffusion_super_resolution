0fd962f50fd702534a809b591864f260c8780386
ordinary813 <ordinaryperson813@gmail.com>
2025-11-15 02:11:22 +0200

    Data preparation file Download kaggle datasets

---
+++ src/prepare_data.py
@@ -0,0 +1,36 @@
+import kagglehub
+import os
+
+def download_div2k():
+    download_dir = "../data" 
+    abs_download_dir = os.path.abspath(download_dir)
+    os.makedirs(download_dir, exist_ok=True)
+
+    # Set the actual download path to the data dir
+    os.environ["KAGGLEHUB_CACHE"] = abs_download_dir
+
+    path = kagglehub.dataset_download(
+        "soumikrakshit/div2k-high-resolution-images",
+        force_download=False
+    )
+    return path
+
+def download_celebA():
+    download_dir = "../data" 
+    abs_download_dir = os.path.abspath(download_dir)
+    os.makedirs(download_dir, exist_ok=True)
+
+    # Set the actual download path to the data dir
+    os.environ["KAGGLEHUB_CACHE"] = abs_download_dir
+
+    path = kagglehub.dataset_download(
+        "badasstechie/celebahq-resized-256x256",
+        force_download=False
+    )
+    return path
+
+if __name__ == "__main__":
+    path = download_div2k()
+    print(f"div2k dataset is located at: {path}")
+    path = download_celebA()
+    print(f"celebA dataset is located at: {path}")
